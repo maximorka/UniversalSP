dbc62139e68b583b910f05578fda4659a66efd2a
maximorka <maximorka@gmail.com>
2020-05-05 12:57:53 +0300

    third com1

--- src/main/java/comUniversal/lowLevel/EthernetDriver.java
+++ src/main/java/comUniversal/lowLevel/EthernetDriver.java
@@ -1,7 +1,18 @@
 package comUniversal.lowLevel;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class EthernetDriver {
-    private  int test = 0;
+    private OutputStream outputStream;
+    private InputStream inputStream;
+    private String ip;
+    private int port;
+
+//    private InputThread inputThread;
+//    private OutputThread outputThread;
+
+
 
 
 

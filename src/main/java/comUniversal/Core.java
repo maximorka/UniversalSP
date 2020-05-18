1e68edd5c488dc96f64aeadfdfd7e447c35e4045
Ihor <maximorka@gmail.com>
2020-05-18 15:25:35 +0300

    Create modulator PSK, add to Core

--- src/main/java/comUniversal/Core.java
+++ src/main/java/comUniversal/Core.java
@@ -1,20 +1,26 @@
 package comUniversal;
 
+import comUniversal.lowLevel.BufferController.BufferController;
+import comUniversal.lowLevel.BufferController.GetIQSource;
+import comUniversal.lowLevel.BufferController.IBufferController;
 import comUniversal.lowLevel.DriverEthernet.EthernetDriver;
-import comUniversal.lowLevel.DriverHorizon.DriverHorizon;
+import comUniversal.lowLevel.DriverHorizon.*;
+import comUniversal.lowLevel.Modulator.ModulatorPsk;
 import comUniversal.ui.ReceiverUPSWindowUI;
 import comUniversal.ui.TransiverUPSWindow;
 import comUniversal.ui.TransmitterUPSWindowUI;
+import comUniversal.util.Complex;
 
 
 public class Core {
     private static Core core = new Core();
-    public EthernetDriver ethernetDriver = new EthernetDriver();
+    public EthernetDriver ethernetDriver;
+    public DriverHorizon driverHorizon;
+    public BufferController bufferController;
+    public ModulatorPsk modulatorPsk;
     public ReceiverUPSWindowUI receiverUPSWindowUI = new ReceiverUPSWindowUI();
     public TransiverUPSWindow transiverUPSWindow = new TransiverUPSWindow();
     public TransmitterUPSWindowUI transmitterUPSWindowUI = new TransmitterUPSWindowUI();
-
-    public DriverHorizon driverHorizon;
     private Update update;
     private boolean running = false;
     /**
@@ -30,6 +36,7 @@ public class Core {
         @Override
         public void run() {
 
+
             while (true) {
                 if(running){
 
@@ -42,9 +49,30 @@ public class Core {
         this.running = running;
     }
     private Core(){
+
+        ethernetDriver = new EthernetDriver();
         driverHorizon = new DriverHorizon();
-        ethernetDriver.addReceiverListener(data -> driverHorizon.parse(data));
+        bufferController = new BufferController(3000);
+        modulatorPsk = new ModulatorPsk();
+
+        modulatorPsk.setRelativeBaudeRate(100.f/3000.f);
+        bufferController.updateSampleFrequency(3000);
+        bufferController.setSources(() -> modulatorPsk.getSempl());
+        bufferController.addTransferListener(sample -> driverHorizon.ducSetIq(sample));
         driverHorizon.addTransferListener(data -> ethernetDriver.writeBytes(data));
+        driverHorizon.addDucBufferPercent(percent -> bufferController.updatePercent(percent));
+        ethernetDriver.addReceiverListener(data -> driverHorizon.parse(data));
+
+        ethernetDriver.doInit("192.168.0.2", 81);
+
+        try {Thread.sleep(1000);} catch (InterruptedException e) {}
+        driverHorizon.ducSetWidth(Width.kHz_3);
+        driverHorizon.ducSetMode(Mode.ENABLE);
+        try {Thread.sleep(1000);} catch (InterruptedException e) {}
+
+
+
+
         update = new Update();
         update.start();
 

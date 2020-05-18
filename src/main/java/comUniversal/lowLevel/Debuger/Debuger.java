f4e9c24be984afa81aef3b1ed78b962c87a9cdf7
vadym <maximorka@gmail.com>
2020-05-18 18:13:08 +0300

    njjn

--- src/main/java/comUniversal/lowLevel/Debuger/Debuger.java
+++ src/main/java/comUniversal/lowLevel/Debuger/Debuger.java
@@ -1,13 +1,7 @@
 package comUniversal.lowLevel.Debuger;
 
-import comUniversal.lowLevel.DriverEthernet.EthernetDriver;
-import comUniversal.lowLevel.DriverEthernet.ReceiverDataBytes;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
